4d4093d53f4e787c0348d74c6849ab989d872233
kamuli3t <kamuli3t@gmail.com>
2025-02-10 08:14:10 -0800

    added roll method to the adventurer

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+//* I'm using JS debug Terminal from VS code.
+
 const adventurer = {
   name: "Robin",
   health: 10,
@@ -12,3 +14,20 @@ for (let i = 0; i < adventurer.inventory.length; i++) {
 adventurer.companion = { name: "Leo", type: "Cat" };
 
 console.log(adventurer);
+
+const leo = adventurer.companion;
+
+leo.companion = {
+  name: "Frank",
+  type: "Flea",
+  belongings: ["hat", "sunglasses"],
+};
+
+adventurer.roll = function (mod = 0) {
+  const result = Math.floor(Math.random() * 20) + 1 + mod;
+  console.log(`${this.name} rolled a ${result}`);
+};
+
+adventurer.roll();
+adventurer.roll();
+adventurer.roll();
